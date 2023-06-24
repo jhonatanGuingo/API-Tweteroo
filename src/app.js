59c4b9e096ddfbc3d8007aa8ad2f8500b67851af
jhonatanGuingo <jhonatan.amaral@engenharia.ufjf.br>
2023-06-24 16:11:14 -0300

    post tweets OK

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 import cors from "cors";
 
 const app = express();
@@ -6,25 +6,33 @@ app.use(cors());
 app.use(express.json());
 const usuario = [];
 const tweets = [];
-app.get('/tweets', (req, res) => {
-    res.send('retorna os 10 últimos tweets');
-})
+app.get("/tweets", (req, res) => {
+  res.send("retorna os 10 últimos tweets");
+});
 
-app.post('/sign-up', (req, res) => {
-    usuario.push(req.body)
-    res.send("OK")
-    console.log(usuario)
-})
+app.post("/sign-up", (req, res) => {
+  usuario.push(req.body);
+  res.send("OK");
+  console.log(usuario);
+});
 
-app.post('/tweets', (req, res) => {
-    const {username} = req.params;
-    const usuarioRegistrado = usuario.find((pessoa) => pessoa.username === username);
-    if(usuarioRegistrado){
-        tweets.push(req.body)
-        res.send(tweets)
-        return
-    }
-    res.send('UNAUTHORIZED')
+app.post("/tweets", (req, res) => {
+  const { username } = req.body;
+  console.log(username);
+  const usuarioRegistrado = usuario.find(
+    (pessoa) => pessoa.username === username
+  );
 
-})
-app.listen(5000, () => console.log('server is running on port 5000'));
+  if (usuarioRegistrado) {
+    let tweet = {
+      username: usuarioRegistrado.username,
+      avatar: usuarioRegistrado.avatar,
+      tweet: req.body.tweet,
+    };
+    tweets.push(tweet);
+    res.send(tweets);
+    return;
+  }
+  res.send("UNAUTHORIZED");
+});
+app.listen(5000, () => console.log("server is running on port 5000"));
